d3abf683bb922d2563665244b444cde17d61f7c6
Maryna Karpova <mari.artarea@gmail.com>
2024-02-19 16:25:27 +0100

    make build

---
+++ index.0682f816.js
@@ -0,0 +1,2 @@
+document.querySelector(".form__button").addEventListener("click",function(e){e.preventDefault()}),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add(".menu"):document.body.classList.remove(".menu")});
+//# sourceMappingURL=index.0682f816.js.map
